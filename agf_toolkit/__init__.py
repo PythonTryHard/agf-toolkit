389bd24ff7b0817f5941bfcf28d961a47997d720
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2022-12-10 17:18:37 +0700

    Remove debug print

--- agf_toolkit/__init__.py
+++ agf_toolkit/__init__.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.environ.get("LOGGING_LEVEL", "INFO"))
 os.environ["LOGURU_LEVEL"] = os.environ.get("LOGGING_LEVEL", "INFO")
 
 DATA_DIR = "~/.agf_toolkit"
